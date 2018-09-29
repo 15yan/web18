d3efba72523ae61c38e02bbb784abae7d1c520a9
yan yan <ece.yan@gmail.com>
2018-09-29 20:35:15 +1000

    index commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<form action="controller/pdoLogin.php" method="post">
+    <fieldset>
+        <legend> Login Page</legend>
+        <label>Username:</label>
+        <input type="text" name="username" required>
+        <label>Password:</label>
+        <input type="text" name="password" required>
+        <input type="submit" value=" Submit ">
+        </fieldset>
+</form>
+<a href="view/pages/registration.php"> Register here </a>
